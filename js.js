b3d94e6ac539436640b65bacba6b65efd6f44887
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-06-22 14:11:42 +0530

    22/06/2024 02:11 PM

---
+++ js.js
@@ -0,0 +1,80 @@
+var rid = '1234';
+
+switch(rid){
+case '1234':
+  console.log({
+    'vdzfj':'ifdifhg'
+  });
+  break;
+case '23465':
+  console.log({
+    'duhgfduv':'ffvliufshub'
+  });
+  break;
+default:
+  console.log('error')
+}
+
+if(){
+  
+}
+
+
+// var x = [{
+//   'naem':'hvlzdihf',
+//   'ohvsruidv0':'ivsd8ygu',
+//   'iudg':'rauirdg'
+// },{
+//   'naem':'hvlzdihf',
+//   'ohvsruidv0':'ivsd8ygu',
+//   'iudg':'rauirdg'
+// }]
+
+// var Id = '1234444';
+
+// switch (Id) {
+//   case "1234":
+//     console.log({
+//       'name':'karthik',
+//       'dob':'25/11/1997',
+//     });
+//     break;
+//   case "2345":
+//     console.log({
+//       'name':'Gangadhar Jami',
+//       'dob':'15/10/2003',
+//     });
+//     break;
+//   case "3456":
+//     console.log({
+//       'name':'karthik',
+//       'dob':'25/11/1997',
+//     });
+//     break;
+//   case "4567":
+//     console.log({
+//       'name':'Gangadhar Jami',
+//       'dob':'15/10/2003',
+//     });
+//     break;
+//   case "5678":
+//     console.log({
+//       'name':'karthik',
+//       'dob':'25/11/1997',
+//     });
+//     break;
+//   case "6789":
+//     console.log({
+//       'name':'Gangadhar Jami',
+//       'dob':'15/10/2003',
+//     });
+//     break;
+//   case "7890":
+//     console.log({
+//       'name':'karthik',
+//       'dob':'25/11/1997',
+//     });
+//     break;
+//   default:
+//     console.log("Invalid User I'd");
+// }
